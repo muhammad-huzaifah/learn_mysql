78f0240f143131b4b30dd5aad2906a42185a06cc
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-03-22 14:21:10 +0700

    add file belajar_mysql for learn mysql

--- belajar_mysql.sql
+++ belajar_mysql.sql
@@ -1,42 +1,59 @@
-/*
- Navicat MariaDB Data Transfer
+CREATE TABLE barang(
+                       id INT,
+                       nama  VARCHAR(100),
+                       harga INT,
+                       jumlah INT
+)ENGINE = innoDB;
+SHOW TABLES ;
+DESCRIBE barang;
+SHOW CREATE TABLE barang;
+ALTER TABLE barang
+    ADD COLUMN deskripsi TEXT;
+ALTER TABLE barang
+    ADD COLUMN salah TEXT;
+ALTER TABLE barang
+    MODIFY nama VARCHAR(200) AFTER salah;
+ALTER TABLE barang
+    MODIFY nama VARCHAR(200) FIRST;
+DESCRIBE barang;
+ALTER TABLE barang
+    MODIFY id INT NOT NULL ;
+ALTER TABLE barang
+    MODIFY nama VARCHAR(200) NOT NULL ;
+SHOW CREATE TABLE barang;
+ALTER TABLE barang
+    MODIFY harga INT NOT NULL DEFAULT 0;
+ALTER TABLE barang
+    MODIFY jumlah INT NOT NULL DEFAULT 0;
+ALTER TABLE barang
+    ADD COLUMN waktu_dibuat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+INSERT INTO barang (id, nama) VALUES (1, 'Ujeb');
+SELECT * FROM barang;
+TRUNCATE TABLE barang;
+SHOW TABLES ;
+DROP TABLE barang;
+CREATE TABLE products(
+                         id VARCHAR(10) NOT NULL,
+                         name VARCHAR(100) NOT NULL,
+                         description TEXT,
+                         price INT UNSIGNED NOT NULL,
+                         quantity INT UNSIGNED NOT NULL DEFAULT 0,
+                         create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB;
+DESCRIBE products;
+INSERT INTO products(id, name, price, quantity)
+VALUES ('P0001', 'Mie Ayam Original', 15000, 100);
+SELECT * FROM products;
+INSERT INTO products(id, name, description, price, quantity)
+VALUES ('P0002', 'Mie Ayam Bakso Tahu', 'Mie Ayam Original + Bakso Tahu', 200000, 100);
+INSERT INTO products(id, name, price, quantity)
+VALUES ('P0003', 'Mie Ayam Ceker', 20000, 100),
+       ('P0004', 'Mie Ayam Spesial', 25000, 100),
+       ('P0005', 'Mie Ayam Yamin', 15000, 100);
 
- Source Server         : LOCALHOST
- Source Server Type    : MariaDB
- Source Server Version : 100413
- Source Host           : localhost:3306
- Source Schema         : belajar_mysql
+SELECT id, name, price, quantity FROM products;
 
- Target Server Type    : MariaDB
- Target Server Version : 100413
- File Encoding         : 65001
+DELETE FROM products WHERE id='P0001';
+INSERT INTO products(id, name, price, quantity)
+VALUES('P0001', 'Mie Ayam Original', 15000, 100);
 
- Date: 15/03/2021 10:52:07
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for products
--- ----------------------------
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products`  (
-                             `id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-                             `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-                             `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-                             `price` int(10) UNSIGNED NOT NULL,
-                             `quantity` int(10) UNSIGNED NOT NULL DEFAULT 0,
-                             `create_at` timestamp NOT NULL DEFAULT current_timestamp
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of products
--- ----------------------------
-INSERT INTO `products` VALUES ('P0002', 'Mie Ayam Bakso Tahu', 'Mie Ayam Original + Bakso Tahu', 200000, 100, '2021-03-11 09:34:13');
-INSERT INTO `products` VALUES ('P0003', 'Mie Ayam Ceker', NULL, 20000, 100, '2021-03-11 09:36:41');
-INSERT INTO `products` VALUES ('P0004', 'Mie Ayam Spesial', NULL, 25000, 100, '2021-03-11 09:36:41');
-INSERT INTO `products` VALUES ('P0005', 'Mie Ayam Yamin', NULL, 15000, 100, '2021-03-11 09:36:41');
-INSERT INTO `products` VALUES ('P0001', 'Mie Ayam Original', NULL, 15000, 100, '2021-03-11 09:55:24');
-
-SET FOREIGN_KEY_CHECKS = 1;
